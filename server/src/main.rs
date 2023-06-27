a098ea2b442044bc45a0cc4bf3f33f6b9f255793
sovnya <enp7s1@tutanota.com>
2023-06-27 23:29:15 +0200

    feat: Add basic HTTP server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use axum::http::Method;
+use axum::{response::Html, routing::get, Router};
+use tower_http::cors::Any;
+use tower_http::cors::CorsLayer;
+
+#[tokio::main]
+async fn main() {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any);
+
+    let app = Router::new()
+        .route("/", get(index))
+        // .route("/ws", get(websocket::websocket_handler))
+        .layer(cors);
+
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// for now
+async fn index() -> Html<&'static str> {
+    Html("I have to code this.")
 }
