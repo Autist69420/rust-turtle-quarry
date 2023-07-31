77689590447ffcbeef0fd8b6f2f987faf6a6cf9f
sovnya <enp7s1@tutanota.com>
2023-08-01 01:09:28 +0200

    feat(server): Add debug logs

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,11 +2,17 @@ use axum::http::Method;
 use axum::{response::Html, routing::get, Router};
 use tower_http::cors::Any;
 use tower_http::cors::CorsLayer;
+use tower_http::trace::TraceLayer;
 
 use server::websocket;
 
 #[tokio::main]
 async fn main() {
+    // Setup logging
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
     let cors = CorsLayer::new()
         .allow_methods([Method::GET, Method::POST])
         .allow_origin(Any);
@@ -14,6 +20,7 @@ async fn main() {
     let app = Router::new()
         .route("/", get(index))
         .route("/ws/", get(websocket::websocket_handler))
+        .layer(TraceLayer::new_for_http())
         .layer(cors);
 
     axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
