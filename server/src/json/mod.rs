ec8bf57f861ad4cd13c21f0ebb85963fa3c0ea26
sovnya <enp7s1@tutanota.com>
2023-08-01 13:53:41 +0200

    feat(server): Implement display trait for position

--- server/src/json/mod.rs
+++ server/src/json/mod.rs
@@ -1,25 +1,52 @@
+use std::fmt::Display;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Position(i32, i32, i32);
+
+impl Display for Position {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Pos({}, {}, {})", self.0, self.1, self.2)
+    }
+}
 
 /// From client to server
 #[derive(Deserialize, Debug)]
 #[serde(tag = "type", content = "data")]
 pub enum CCPacket {
     /// Sent when a client connects.
-    ClientConnect { name: String, id: u64 },
+    ClientConnect { name: String, id: u64, gps: Position },
     /// Sent when a turtle moves down
+    ///
+    /// # Paramaters
+    /// - `old_level` - The old layer level
+    /// - `new_level` - The new layer level
+    /// - `position` - Relative coords from the original GPS location
     LevelDecrease {
         old_level: i32,
         new_level: i32,
         position: Position,
     },
     /// Sent when a turtle moves up
+    ///
+    /// # Paramaters
+    /// - `old_level` - The old layer level
+    /// - `new_level` - The new layer level
+    /// - `position` - Relative coords from the original GPS location
     LevelIncrease {
         old_level: i32,
         new_level: i32,
         position: Position,
+    },
+    /// Sent when a turtle moves
+    ///
+    /// # Paramaters
+    /// - `old_position` - Relative coords from the original GPS location
+    /// - `new_position` - Relative coords from the original GPS location
+    UpdatePosition {
+        new_position: Position,
+        old_position: Position,
     },
 }
 
@@ -30,10 +57,5 @@ pub enum ServerPacket {
     Acknowledge {
         message: String,
         ok: bool,
-    },
-    /// Sent every second to update the "database"
-    UpdatePosition {
-        new_position: Position,
-        old_position: Position,
     },
 }
